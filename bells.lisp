eb4d21dfccaa31c3604ddfe9f5280d419aba0b93
arnaiz <arnaiz@boimboim.com>
2017-01-02 05:27:24 +0100

    shorter version

--- bells.lisp
+++ bells.lisp
@@ -1,13 +1,6 @@
-;arnaiz 12/12/2016
-;
-;schedule launch at 1/1/YR 00:00:00
-;
 (defconstant YR 2017)
-(defvar bell 1)
-(loop
-	(format t "~C~2d" (code-char 7) bell)
-	(terpri)
-	(sleep 3)
-	(incf bell 1)
-(when (>= bell 12)(return bell)))
+(dotimes (bell 12)
+  (format t "~2d~%~C" (+ bell 1) (code-char 7))
+  (sleep 3) 
+)
 (format t "FELIZ ~4d!!!" YR)
